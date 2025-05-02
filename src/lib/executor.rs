31417602356a1fdf0bd90bc7cf7b68da58f857e0
Aman Rao <amanrao@rapidinnovation.dev>
2025-05-02 23:49:51 +0530

    Added access to the executor in Task struct

--- src/lib/executor.rs
+++ src/lib/executor.rs
@@ -1,43 +1,62 @@
+use std::collections::HashMap;
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Wake, Waker};
+use std::task::{Context, Poll, Waker};
 use std::pin::Pin;
 
+use futures::task::{waker, ArcWake};
 use polling::Poller;
 
-struct Task{
+pub struct Task{
     future: Mutex<Option<Pin<Box<dyn Future<Output = ()> + Send + Sync>>>>,
     sender: Sender<Arc<Task>>,
+    executor: Arc<Executor>,
 }
 
-impl Wake for Task {
-    fn wake(self: Arc<Self>) {
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
         // Notify the executor to poll this task again
-        let _ = self.sender.send(self.clone());
+        let _ = arc_self.sender.send(arc_self.clone());
     }
 }
 
 struct Executor {
-    task_queue: Receiver<Arc<Task>>,
-    poller: Poller
+    task_queue: Mutex<Receiver<Arc<Task>>>,
+    poller: Poller,
+    event_map: Arc<HashMap<usize, Waker>>,
 }
 
 impl Executor {
-    fn new(task_queue: Receiver<Arc<Task>>) -> Self {
+    fn new(task_queue: Receiver<Arc<Task>>, event_map: Arc<HashMap<usize, Waker>>) -> Self {
         let poller = Poller::new().expect("Failed to create poller");
-        Executor { task_queue, poller }
+        let task_queue = Mutex::new(task_queue);
+        Executor { task_queue, poller, event_map }
     }
 
     fn run(&mut self) {
         loop {
             // Wait for tasks to be available
-            if let Ok(task) = self.task_queue.try_recv() {
+            if let Ok(task) = self.task_queue.lock().unwrap().try_recv() {
+
+                // Get MutexGuard for the task's future. Need the mutex to mutate Arc.
                 let mut locked_future = task.future.lock().unwrap();
+
+                // Temporarily take the future out of Option
                 if let Some(mut owned_task) = locked_future.take() {
-                    let waker = Waker::from(task.clone());
+                    let waker = waker(task.clone());
                     let mut context = Context::from_waker(&waker);
 
-                    let _result = owned_task.as_mut().poll(&mut context);
+                    *locked_future = match owned_task.as_mut().poll(&mut context) {
+                        Poll::Pending => {
+                            // Task is not ready, put it back in the queue
+                            Some(owned_task)
+                        },
+
+                        Poll::Ready(_) => {
+                            // Task is complete, drop it
+                            None
+                        }
+                    };
                 }
             }
             
